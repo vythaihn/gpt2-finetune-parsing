4782bf6e4ef610ee3fa023b33e882b6bce7fee34
vythaihn <vythai@stanford.edu>
2021-11-19 13:51:17 -0800

    changes

---
+++ get_sent_prob.py
@@ -0,0 +1,248 @@
+from torch.optim.lr_scheduler import ExponentialLR
+from transformers import GPT2Tokenizer
+#get pretrained tokenizer
+from transformers import AutoTokenizer
+"""
+tokenizer = GPT2Tokenizer.from_pretrained('stanza_dataset/tokenized_data')
+"""
+from torch.utils.data import Dataset
+import torch
+import datetime
+import numpy as np
+from torch.utils.data import random_split
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
+from transformers import GPT2LMHeadModel, GPT2Config, GPTNeoForCausalLM, GPTNeoConfig
+#configuration = GPT2Config.from_pretrained('gpt2', output_hidden_states=False)
+import argparse
+"""
+tokenizer.add_special_tokens({
+  "eos_token": "<EOS>",
+  "bos_token": "<BOS>",
+  "unk_token": "<unk>",
+  "pad_token": "<PAD>",
+  "mask_token": "<mask>"
+})
+"""
+
+
+def tokenize_seq(sent, tokenizer, max_length):
+    return tokenizer(sent, truncation=True, max_length=max_length, padding="max_length")
+class ParsingDataset(Dataset):
+
+    def __init__(self, sentences, tokenizer, tokenizer_type="bert", max_length=1000):
+        self.tokenizer = tokenizer
+        self.input_ids = []
+        self.attn_masks = []
+
+        for sentence in sentences:
+            if tokenizer_type=="bert":
+                encodings = tokenize_seq(sentence, tokenizer, max_length)
+            else:
+                encodings = tokenize_seq("<s> " + sentence + " </s>", tokenizer, max_length)
+            #print(count)
+            #count+=1
+            input_id = [0 for v in encodings['input_ids'] if v is None]
+            if encodings['input_ids'][max_length-1] in (tokenizer.pad_token_id, tokenizer.eos_token_id) and input_id==[]:
+                self.input_ids.append(torch.tensor(encodings['input_ids']))
+                self.attn_masks.append(torch.tensor(encodings['attention_mask']))
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.attn_masks[idx]
+
+
+def format_time(elapsed):
+    return str(datetime.timedelta(seconds=int(round((elapsed)))))
+
+import gc
+gc.collect()
+#evaluate function
+
+
+    # Training settings
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+#train epoch for now
+
+def process_data(args, filename):
+    new_token_list = set()
+    with open(filename) as file:
+        lines = file.readlines()
+        all_sentences = [line.strip() for line in lines]
+        new_sentences = []
+        new_sentences_bert = []
+
+        for each_sent in all_sentences:
+            if each_sent != "":
+                words = each_sent.split()
+                if len(words) < args.max_length:
+                    new_tokens = {word for word in words if ("(_" in word or ")_" in word)}
+                    # print(new_tokens)
+                    new_token_list.update(new_tokens)
+                    if args.tokenizer =="tokenizer/tokenizer_bert":
+                        new_sentences_bert.append(each_sent)
+                    else:
+                        new_sent = ""
+                        for word in words:
+                            if ("(_" not in word and ")_" not in word):
+                                new_sent += word.replace("_"," ") + " "
+                            else:
+                                new_sent += word + " "
+                        new_sentences.append(new_sent.strip())
+
+    data = new_sentences_bert if args.tokenizer == "tokenizer/tokenizer_bert" else new_sentences
+
+    return new_token_list, data
+
+def main():
+    parser = argparse.ArgumentParser(description='CIFAR-10 dataset')
+    parser.add_argument('--batch-size', type=int, default=4,
+                        help='input batch size for training')
+    parser.add_argument('--data-gold', default='stanza_dataset/gold/',
+                        help='directory that contains cifar-10-batches-py/ '
+                             '(downloaded automatically if necessary)')
+    parser.add_argument('--data-silver', default='stanza_dataset/silver/',
+                        help='directory that contains cifar-10-batches-py/ '
+                             '(downloaded automatically if necessary)')
+    parser.add_argument('--epochs', type=int, metavar='N',
+                        help='number of epochs to train')
+    parser.add_argument('--lr', type=float, metavar='LR', default=0.00005,
+                        help='learning rate')
+    parser.add_argument('--save-model', default="test",
+                        help='saves the current model at path')
+    parser.add_argument('--continue-train', action='store_true', default=False,
+                        help='saves the current model at path')
+    parser.add_argument('--tokenizer', default="tokenizer/tokenizer_bert",
+                        help='saves the current model at path')
+    parser.add_argument('--model-name', default="gpt2", choices=["gpt2", "gpt-neo-vi-small", "gpt2-viwiki"],
+                        help='saves the current model at path')
+    parser.add_argument('--train', action='store_true', default=False,
+                        help='saves the current model at path')
+    parser.add_argument('--eval', action='store_true', default=False,
+                        help='saves the current model at path')
+    parser.add_argument('--create-tokenizer', default=False, action='store_true',
+                        help='saves the current model at path')
+    parser.add_argument('--max-length', default=512, type=int,
+                        help='saves the current model at path')
+    args = parser.parse_args()
+
+    if args.train:
+        log_file = open("saved_model/" + "logging_" + args.save_model + "_" + args.model_name + '.pt', 'w')
+        log_file.write("hello world!")
+    else:
+        log_file = open("saved_model/" + "logging_continued_" + args.save_model + "_" + args.model_name + '.pt', 'w')
+
+    log_file.flush()
+
+    def eval_keywords(keywords):
+        model.eval()
+        for keyword in keywords:
+            input_seq = keyword if args.tokenizer=="tokenizer/tokenizer_bert" else "<s> " + keyword
+
+            generated = torch.tensor(tokenizer.encode(input_seq)).unsqueeze(0)
+
+            #print(generated)
+            generated = generated.to(device)
+            sample_outputs = model.generate(
+                generated,
+                do_sample=True,
+                top_k=30,
+                max_length=600,
+                top_p=0.90,
+                num_return_sequences=2
+            )
+            for i, sample_output in enumerate(sample_outputs):
+                print("{}: {}".format(i, tokenizer.decode(sample_output, skip_special_tokens=True)))
+                log_file.write("{}: {} \n".format(i, tokenizer.decode(sample_output, skip_special_tokens=True)))
+
+    def sent_scoring(sentence, tokenizer_type):
+        assert model is not None
+        assert tokenizer is not None
+        if tokenizer_type == "bert":
+            encodings = torch.tensor(tokenizer.encode(sentence, truncation = True, max_length = 1000)).unsqueeze(0)
+        else:
+            encodings = torch.tensor(tokenizer.encode("<s> " + sentence + " </s>", truncation = True, max_length = 1000)).unsqueeze(0)
+
+        error = [0 for v in encodings['input_ids'] if v is None]
+
+        if error == []:
+            # input_ids = torch.tensor(tokenizer.encode(text)).unsqueeze(0)  # Batch size 1
+            input_ids = encodings.to('cuda')
+            with torch.no_grad():
+                outputs = model(input_ids, labels=input_ids)
+            loss, logits = outputs[:2]
+            sentence_prob = loss.item()
+            return np.exp(sentence_prob.numpy())
+        else:
+            return float("inf")
+
+
+
+    keywords = ["(_ROOT (_S (_NP", "(_ROOT (_S (_S", "(_ROOT (_S (_VP", "(_ROOT", "(_ROOT"]
+
+    tok_type = "bert" if args.tokenizer == "tokenizer/tokenizer_bert" else "difff"
+
+
+    if args.model_name=="gpt-neo-vi-small":
+        tokenizer = GPT2Tokenizer.from_pretrained(args.tokenizer)
+
+    else:
+        tokenizer = AutoTokenizer.from_pretrained(args.tokenizer)
+
+    configuration_GPT2 = GPT2Config(
+        vocab_size=tokenizer.vocab_size,
+        bos_token_id=tokenizer.bos_token_id,
+        eos_token_id=tokenizer.eos_token_id
+    )
+
+    """
+    configuration_GPT2_neo = GPTNeoConfig(
+        vocab_size=tokenizer.vocab_size,
+        bos_token_id=tokenizer.bos_token_id,
+        eos_token_id=tokenizer.eos_token_id
+    )
+    """
+
+    configuration_GPT2_neo = GPTNeoConfig.from_pretrained("NlpHUST/gpt-neo-vi-small", output_hidden_states=False)
+    configuration_GPT2_neo.bos_token_id = tokenizer.bos_token_id
+    configuration_GPT2_neo.eos_token_id = tokenizer.eos_token_id
+    configuration_GPT2_neo.vocab_size = tokenizer.vocab_size
+
+    if args.model_name=="gpt2":
+        model = GPT2LMHeadModel.from_pretrained("gpt2", config=configuration_GPT2)
+    elif args.model_name=="gpt-neo-vi-small":
+        model = GPTNeoForCausalLM.from_pretrained("NlpHUST/gpt-neo-vi-small",config=configuration_GPT2_neo)
+    elif args.model_name=="gpt2-viwiki":
+        model = GPT2LMHeadModel.from_pretrained('danghuy1999/gpt2-viwiki', config=configuration_GPT2)
+
+    # Notice: resize_token_embeddings expect to receive the full size of the new vocabulary, i.e., the length of the tokenizer.
+    # model.resize_token_embeddings(len(tokenizer))
+    model.resize_token_embeddings(len(tokenizer))
+    model = model.to(device)
+    model.eval()
+
+    sentences = []
+
+
+    if args.eval:
+        model.load_state_dict(torch.load("saved_model/" +  + args.save_model  + "_" + args.model_name + '.pt'))
+        """
+        TODO:
+        """
+        min_score = float("inf")
+        best_sent = None
+        for idx, sent in enumerate(sentences):
+            score = sent_scoring(sent, tokenizer_type=tok_type)
+            if score < min_score:
+                min_score = score
+                best_sent = idx
+
+    log_file.close()
+if __name__ == "__main__":
+    main()
+
+
